808d4f6d8fa17f1a99e1fc87a9b780fd0135919d
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-01-18 11:09:51 -0800

    started problem 9

--- hw1/_build/default/bin/main.ml
+++ hw1/_build/default/bin/main.ml
@@ -28,7 +28,6 @@ Thus, the self_member is not possible to implement in OCaml.
 let rec computed_fixed_point eq f x = if (eq) x (f x) then x else computed_fixed_point eq f (f x)
 
 (* 7 *)
-(*   computed_periodic_point (=) x^2-1 2 0.5 = -1. *)
 let rec is_computed_periodic_point eq f p x start = match p with 
 | 0 -> (eq) x start
 | _ -> is_computed_periodic_point eq f (p-1) (f x) start
@@ -37,15 +36,107 @@ let rec computed_periodic_point eq f p x =
   if is_computed_periodic_point eq f p x x then x
   else computed_periodic_point eq f p (f x)
 
+(* 8 *)
+let rec whileseq_helper s p l = match l with
+| [] -> []
+| x::_ -> let next = s x in 
+  if (p next) then whileseq_helper s p (next::l)
+  else l
+
+let whileseq s p x = List.rev (whileseq_helper s p [x])
+
+(* 9 *)
+
+(* let rec get_outgoing_edges key l = match l with
+| [] -> []
+| (k, v)::t -> if key = k 
+    then v :: get_outgoing_edges key t
+    else get_outgoing_edges key t
+
+let get_outgoing_edges key l = List.map (fun (_, v) -> v) (List.filter (fun (k, _) -> k = key) l) *)
+
+let get_outgoing_edges key l = List.fold_right (fun (k,v) acc -> if k = key then v::acc else acc) l []
+
+type ('nonterminal, 'terminal) symbol =
+  | N of 'nonterminal
+  | T of 'terminal
+
+
+let rec is_blind_alley node grammar visited = match node with
+  | T _ -> false
+  | N n -> if List.mem n visited then true else 
+            let new_visited = n::visited in
+            let outgoing_edges = get_outgoing_edges n grammar in
+            not (List.exists (fun next_expr -> 
+              List.exists (fun symbol -> is_blind_alley symbol grammar new_visited) next_expr
+            ) outgoing_edges)
+
+(* Test cases
+let test_get_outgoing_edges () =
+  (* Test data *)
+  let edges = [("a", 1); ("b", 2); ("a", 3); ("c", 4); ("a", 5)] in
+
+  (* 1. Basic functionality *)
+  assert (get_outgoing_edges "a" edges = [1; 3; 5]);
+
+  (* 2. No matching key *)
+  assert (get_outgoing_edges "d" edges = []);
+
+  (* 3. Empty list *)
+  assert (get_outgoing_edges "a" [] = []);
+
+  (* 4. Repeated keys *)
+  assert (get_outgoing_edges "b" edges = [2]);
+
+  (* If no assertion fails, all tests pass *)
+  print_endline "All tests passed."
+
+(* Run the test cases *)
+let () = test_get_outgoing_edges () *)
+
+
+(* Test Cases *)
+
+type awksub_nonterminals =
+| Expr | Num
+
+let test_is_blind_alley () =
+  (* Test Grammars *)
+  let grammar1 = [
+    Expr, [T"("; N Num; T")"];
+    Num, [T"0"];
+  ] in
+  let grammar2 = [
+    Expr, [N Expr];
+  ] in
+  (* Test 1: No cycle *)
+  assert (is_blind_alley (N Expr) grammar1 [] = false);
+  assert (is_blind_alley (N Expr) grammar2 [] = true);
+
+  (* If no assertion fails, all tests pass *)
+  print_endline "All tests passed."
+
+(* Run the test cases *)
+let () = test_is_blind_alley ()
+
+
+
+
+
+
+
+
+
+(* TESTS *)
 let computed_periodic_point_test0 =
   computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1
 let computed_periodic_point_test1 =
   computed_periodic_point (=) (fun x -> x *. x -. 1.) 2 0.5 = -1.
+let whileseq_test = whileseq ((+) 3) ((>) 10) 0 = [0; 3; 6; 9]
   
 let () = print_endline (string_of_bool computed_periodic_point_test0)
 let () = print_endline (string_of_bool computed_periodic_point_test1)
-
-
+let () = print_endline (string_of_bool whileseq_test)
 
 let computed_fixed_point_test0 =
   computed_fixed_point (=) (fun x -> x / 2) 1000000000 = 0
