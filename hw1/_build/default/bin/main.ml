825c3a7622e4dabae4ee6bd97f3e865cd358a1c5
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-01-19 15:07:58 -0800

    test cases

--- hw1/_build/default/bin/main.ml
+++ hw1/_build/default/bin/main.ml
@@ -43,7 +43,7 @@ let rec whileseq_helper s p l = match l with
   if (p next) then whileseq_helper s p (next::l)
   else l
 
-let whileseq s p x = List.rev (whileseq_helper s p [x])
+let whileseq s p x = if p x then List.rev (whileseq_helper s p [x]) else []
 
 (* 9 *)
 
@@ -66,25 +66,49 @@ let filter_blind_alleys (root, rules) = (root, List.filter (fun (_, rhs) -> List
 
 
 (* TESTS *)
-let computed_periodic_point_test0 =
-  computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1
-let computed_periodic_point_test1 =
-  computed_periodic_point (=) (fun x -> x *. x -. 1.) 2 0.5 = -1.
-let whileseq_test = whileseq ((+) 3) ((>) 10) 0 = [0; 3; 6; 9]
-  
-let () = print_endline (string_of_bool computed_periodic_point_test0)
-let () = print_endline (string_of_bool computed_periodic_point_test1)
-let () = print_endline (string_of_bool whileseq_test)
 
+(* 1 subset *)
+let subset_test0 = subset [] []
+let subset_test1 = subset [1;1;1;1] [2;1]
+let subset_test2 = not (subset [1;3;7] []);;
+assert (subset_test0 && subset_test1 && subset_test2)
+
+(* 2 equal_sets *)
+let equal_sets_test0 = equal_sets [1;2;3] [3;3;2;1]
+let equal_sets_test1 = not (equal_sets [-1;-2;-5] [3;1;3])
+let equal_sets_test2 = equal_sets [] [];;
+assert (equal_sets_test0 && equal_sets_test1 && equal_sets_test2)
+
+
+(* 3 set_union *)
+let set_union_test0 = equal_sets (set_union [] []) []
+let set_union_test1 = equal_sets (set_union [4;4;-1] [1]) [4;-1;1;-1];;
+assert (set_union_test0 && set_union_test1)
+
+
+(* 4 set_all_union *)
+let set_all_union_test0 = not (equal_sets (set_all_union [[2;2;2]; [4;4]; [2;-1]]) [4;2;-1;-1;0])
+let set_all_union_test1 =
+  equal_sets (set_all_union [[2;2;2]; [4;4]; [0;2;-1]]) [4;2;-1;-1;0];;
+assert (set_all_union_test0 && set_all_union_test1)
+
+(* 6 computed_fixed_point *)
 let computed_fixed_point_test0 =
-  computed_fixed_point (=) (fun x -> x / 2) 1000000000 = 0
-let () = print_endline (string_of_bool computed_fixed_point_test0)
-let set_all_union_test0 =
-  equal_sets (set_all_union []) []
-let () = print_endline (string_of_bool set_all_union_test0)
+  computed_fixed_point (=) (fun x -> x *. x) 0.5 = 0.0;;
+assert computed_fixed_point_test0;;
 
-(* 10 *)
 
+(* 7 computed_periodic_point *)
+let computed_periodic_point_test0 =
+  computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1;;
+assert computed_periodic_point_test0
+
+(* 8 whileseq *)
+let whileseq_test0 = whileseq ((+) 1) ((<) 5) (2)  = []
+let whileseq_test1 = whileseq ((+) 1) ((>) 5) (2)  = [2;3;4];;
+assert(whileseq_test0 && whileseq_test1)
+
+(* 9 filter_blind_alleys *)
 type awksub_nonterminals = Expr | Num
 let grammar1 = [ Expr, [T"("; N Num; T")"]; Num, [T"0"];]
 let grammar2 = [ Expr, [N Expr];]
@@ -92,8 +116,6 @@ let grammar3 = [ Expr, [T"("; N Num; T")"]; Num, [T"0"; N Num]; Num, [T"1";] ]
 let my_filter_blind_alleys_test0 = filter_blind_alleys (Expr, grammar1) = (Expr, grammar1)
 let my_filter_blind_alleys_test1 = filter_blind_alleys (Expr, grammar2) = (Expr, [])
 let my_filter_blind_alleys_test2 = filter_blind_alleys (Expr, grammar3) = (Expr, grammar3);;
-
 assert (my_filter_blind_alleys_test0 && my_filter_blind_alleys_test1 && my_filter_blind_alleys_test2);;
 
-(* If no assertion fails, all tests pass *)
-print_endline "All tests passed.";;
+
