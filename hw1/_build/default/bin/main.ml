5c1e954ee01bd2b3bdd878cbb9c2a7e35429c678
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-01-25 21:00:30 -0800

    hw 2 part 1 + 2

--- hw1/_build/default/bin/main.ml
+++ hw1/_build/default/bin/main.ml
@@ -70,43 +70,38 @@ let filter_blind_alleys (root, rules) = (root, List.filter (fun (_, rhs) -> List
 (* 1 subset *)
 let subset_test0 = subset [] []
 let subset_test1 = subset [1;1;1;1] [2;1]
-let subset_test2 = not (subset [1;3;7] []);;
-assert (subset_test0 && subset_test1 && subset_test2)
+let subset_test2 = not (subset [1;3;7] [])
 
 (* 2 equal_sets *)
 let equal_sets_test0 = equal_sets [1;2;3] [3;3;2;1]
 let equal_sets_test1 = not (equal_sets [-1;-2;-5] [3;1;3])
-let equal_sets_test2 = equal_sets [] [];;
-assert (equal_sets_test0 && equal_sets_test1 && equal_sets_test2)
+let equal_sets_test2 = equal_sets [] []
 
 
 (* 3 set_union *)
 let set_union_test0 = equal_sets (set_union [] []) []
-let set_union_test1 = equal_sets (set_union [4;4;-1] [1]) [4;-1;1;-1];;
-assert (set_union_test0 && set_union_test1)
+let set_union_test1 = equal_sets (set_union [4;4;-1] [1]) [4;-1;1;-1]
+
 
 
 (* 4 set_all_union *)
 let set_all_union_test0 = not (equal_sets (set_all_union [[2;2;2]; [4;4]; [2;-1]]) [4;2;-1;-1;0])
 let set_all_union_test1 =
-  equal_sets (set_all_union [[2;2;2]; [4;4]; [0;2;-1]]) [4;2;-1;-1;0];;
-assert (set_all_union_test0 && set_all_union_test1)
+  equal_sets (set_all_union [[2;2;2]; [4;4]; [0;2;-1]]) [4;2;-1;-1;0]
 
-(* 6 computed_fixed_point *)
+  (* 6 computed_fixed_point *)
 let computed_fixed_point_test0 =
-  computed_fixed_point (=) (fun x -> x *. x) 0.5 = 0.0;;
-assert computed_fixed_point_test0;;
+  computed_fixed_point (=) (fun x -> x *. x) 0.5 = 0.0
 
 
 (* 7 computed_periodic_point *)
 let computed_periodic_point_test0 =
-  computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1;;
-assert computed_periodic_point_test0
+  computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1
+
 
 (* 8 whileseq *)
 let whileseq_test0 = whileseq ((+) 1) ((<) 5) (2)  = []
-let whileseq_test1 = whileseq ((+) 1) ((>) 5) (2)  = [2;3;4];;
-assert(whileseq_test0 && whileseq_test1)
+let whileseq_test1 = whileseq ((+) 1) ((>) 5) (2)  = [2;3;4]
 
 (* 9 filter_blind_alleys *)
 type awksub_nonterminals = Expr | Num
@@ -116,6 +111,14 @@ let grammar3 = [ Expr, [T"("; N Num; T")"]; Num, [T"0"; N Num]; Num, [T"1";] ]
 let my_filter_blind_alleys_test0 = filter_blind_alleys (Expr, grammar1) = (Expr, grammar1)
 let my_filter_blind_alleys_test1 = filter_blind_alleys (Expr, grammar2) = (Expr, [])
 let my_filter_blind_alleys_test2 = filter_blind_alleys (Expr, grammar3) = (Expr, grammar3);;
+
+assert (subset_test0 && subset_test1 && subset_test2);;
+assert (equal_sets_test0 && equal_sets_test1 && equal_sets_test2);;
+assert (set_union_test0 && set_union_test1);;
+assert (set_all_union_test0 && set_all_union_test1);;  
+assert computed_fixed_point_test0;;
+assert computed_periodic_point_test0;;
+assert(whileseq_test0 && whileseq_test1);;
 assert (my_filter_blind_alleys_test0 && my_filter_blind_alleys_test1 && my_filter_blind_alleys_test2);;
 
 
---
+++ hw2/bin/main.ml
@@ -0,0 +1,84 @@
+
+type ('nonterminal, 'terminal) symbol =
+  | N of 'nonterminal
+  | T of 'terminal
+let accept_all string = Some string
+let accept_empty_suffix = function
+   | _::_ -> None
+   | x -> Some x
+
+(* An example grammar for a small subset of Awk.
+   This grammar is not the same as Homework 1; it is
+   instead the grammar shown above.  *)
+
+type awksub_nonterminals =
+  | Expr | Term | Lvalue | Incrop | Binop | Num
+
+let awksub_rules =
+  [Expr, [N Term; N Binop; N Expr];
+    Expr, [N Term];
+    Term, [N Num];
+    Term, [N Lvalue];
+    Term, [N Incrop; N Lvalue];
+    Term, [N Lvalue; N Incrop];
+    Term, [T"("; N Expr; T")"];
+    Lvalue, [T"$"; N Expr];
+    Incrop, [T"++"];
+    Incrop, [T"--"];
+    Binop, [T"+"];
+    Binop, [T"-"];
+    Num, [T"0"];
+    Num, [T"1"];
+    Num, [T"2"];
+    Num, [T"3"];
+    Num, [T"4"];
+    Num, [T"5"];
+    Num, [T"6"];
+    Num, [T"7"];
+    Num, [T"8"];
+    Num, [T"9"]]
+ 
+let awksub_grammar = Expr, awksub_rules
+ 
+ 
+let awkish_grammar =
+  (Expr,
+   function
+     | Expr ->
+         [[N Term; N Binop; N Expr];
+          [N Term]]
+     | Term ->
+	 [[N Num];
+	  [N Lvalue];
+	  [N Incrop; N Lvalue];
+	  [N Lvalue; N Incrop];
+	  [T"("; N Expr; T")"]]
+     | Lvalue ->
+	 [[T"$"; N Expr]]
+     | Incrop ->
+	 [[T"++"];
+	  [T"--"]]
+     | Binop ->
+	 [[T"+"];
+	  [T"-"]]
+     | Num ->
+	 [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+	  [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
+
+(* 1 - convert hw1 style grammar to hw2 style grammar *)
+let convert_grammar (root, rules) = 
+  let get_rhs symbol = List.fold_right (fun (k,v) acc -> if k = symbol then v::acc else acc) rules [] in 
+    (root, get_rhs)
+
+(* 2 - parse tree leaves left to right *)
+
+type ('nonterminal, 'terminal) parse_tree =
+  | Node of 'nonterminal * ('nonterminal, 'terminal) parse_tree list
+  | Leaf of 'terminal
+
+let parse_tree_leaves tree = let rec helper acc = (function
+  | Leaf l -> l::acc
+  (* call helper (acc, tree) on all items in the subtree list and add them to accumulator *)
+  | Node (_, subtrees) -> List.fold_left helper acc subtrees) 
+  in List.rev (helper [] tree)
+
