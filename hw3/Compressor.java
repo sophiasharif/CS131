8939c2b54813a433227ad4f619b7cdbf1f85ba27
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-02-14 12:23:24 -0800

    compressor

--- hw3/Compressor.java
+++ hw3/Compressor.java
@@ -2,18 +2,29 @@ import java.io.IOException;
 import java.util.zip.CRC32;
 
 public class Compressor {
+
+    CRC32 crc32 = new CRC32();
+    int bytesRead = 0;
+
+    public void init() {
+
+        // write header
+        byte[] gzipHeader = {31, -117, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, -1};
+        try { 
+            System.out.write(gzipHeader); 
+        } catch (IOException e) {
+            System.err.println("Error in writing header: " + e);
+        }
+    }
+    
     public void compress() {
+        
         try {
-
-            // write header
-            byte[] gzipHeader = {31, -117, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, -1};
-            System.out.write(gzipHeader);
-
             // read all bytes from stdin
             byte[] byteBuffer = System.in.readNBytes(128 * 1024);
+            bytesRead += byteBuffer.length;
 
             // calculate crc32 of input data
-            CRC32 crc32 = new CRC32();
             crc32.update(byteBuffer);
 
             // create thread
@@ -22,19 +33,28 @@ public class Compressor {
             thread.start();
             thread.join();
             t1.write();
-            
-            // prepare crc32 and size of compressed data for gzip trailer
-            byte[] gzipTrailer = new byte[8];
-            Utils.writeInt((int) crc32.getValue(), gzipTrailer, 0);
-            Utils.writeInt((int) byteBuffer.length, gzipTrailer, 4);
-            
-            // write header, compressed data, and trailer to stdout
-            System.out.write(gzipTrailer);
 
 
         } catch (IOException | InterruptedException e) {
             System.out.println("Error: " + e.getMessage());
         }
      
+    }
+
+    public void finish() {
+
+        try {
+            
+            // prepare crc32 and size of compressed data for gzip trailer
+            byte[] gzipTrailer = new byte[8];
+            Utils.writeInt((int) crc32.getValue(), gzipTrailer, 0);
+            Utils.writeInt((int) bytesRead, gzipTrailer, 4);
+            
+            // write header, compressed data, and trailer to stdout
+            System.out.write(gzipTrailer);
+
+        } catch (IOException e) {
+            System.out.println("Error in writing trailer: " + e.getMessage());
+        }
     }
 }
--- hw3/Pigzj.java
+++ hw3/Pigzj.java
@@ -6,7 +6,9 @@ public class Pigzj {
     public static void main(String[] args) {
         
         Compressor compressor = new Compressor();
+        compressor.init();
         compressor.compress();
+        compressor.finish();
         
     }
 
